4e3abaa7f7651b8f610c7a8f35d2be6bd2eda836
huhuirong <xxx>
2018-08-14 15:45:45 +0800

    单选module

--- nps-web/src/admin/npsMgr/questionModule/questionModule.js
+++ nps-web/src/admin/npsMgr/questionModule/questionModule.js
@@ -1,3 +1,42 @@
+/* eslint-disable react/react-in-jsx-scope */
 /**
  * Create by chenpengan on 2018/8/14
  */
+import { Radio} from 'antd';
+import {PureComponent} from "react";
+import React from 'react';
+const RadioGroup = Radio.Group;
+//单选
+export default class Radiomodule extends PureComponent {
+    constructor(props){
+        super(props);
+        this.state={
+            id:props.id,
+            title:props.title,
+            optionA:props.optionA,
+            optionB:props.optionB
+        }
+    }
+    render() {
+        const radioStyle = {
+            display: 'block',
+            height: '30px',
+            lineHeight: '30px',
+        };
+        return (
+            <div>
+                <h3>{this.state.id}.{this.state.title}</h3>
+            <RadioGroup value={this.state.value}>
+                <Radio style={radioStyle} value={1}>{this.state.optionA}</Radio>
+                <Radio style={radioStyle} value={2}>{this.state.optionB}</Radio>
+            </RadioGroup>
+            </div>
+        );
+    }
+}
+
+//多选
+
+//单项填空
+
+//多项填空
