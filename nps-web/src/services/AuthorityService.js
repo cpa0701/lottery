45ab6c84403c94bba1631701b5c8755cab033bab
huhuirong <xxx>
2018-08-14 10:00:47 +0800

    数据区域、权限设置接口

--- nps-web/src/services/AuthorityService.js
+++ nps-web/src/services/AuthorityService.js
@@ -4,10 +4,23 @@ class AuthorityService {
     // 获取权限树
     getAuthTree = async (param) => {
         let url = 'permissions';
-        return await Http.get(url);
+        return await Http.get(url,param);
     };
-
-
+    //新增权限
+    addAuthority = async (param) => {
+        let url = 'permissions';
+        return await Http.post(url, param);
+    };
+    //更新权限
+    updateAuthority = async(param)=>{
+        let url = 'permissions/'+param.id;
+        return await Http.put(url, param);
+    }
+    //删除权限
+    deleteAuthority=async(param)=>{
+        let url = 'permissions/'+param.id;
+        return await Http.delete(url, param);
+    }
 
 
 }
--- nps-web/src/services/DomainService.js
+++ nps-web/src/services/DomainService.js
@@ -1,34 +1,25 @@
-/**
- * Created by chenPengAn on 18/03/07.
- */
 import Http from '../common/Http';
 
 class DomainService {
     // 获取区域树
-    getDomainTree=async(param)=>{
-        var url = 'mock/region/initRegionInfo';
-        return await Http.post(url, param);
+    domainTree = async (param)=>{
+        var url = 'regions';
+        return await Http.get(url,param);
     }
-    // 根据条件获取区域树
-    getDomainTreeByPage=async(param)=>{
-        var url = 'safe/region/initRegionInfoByPage';
+    // 区域新增
+    addDomain = async (param) => {
+        let url = 'regions';
         return await Http.post(url, param);
+    };
+    //区域修改
+    updateDomain=async(param)=>{
+        let url = 'regions/'+param.id;
+        return await Http.put(url, param);
     }
-    // 平台菜单批量删除
-    DelDomain = async (param) => {
-        var url = 'safe/region/deleteRegionInfo';
-        return await Http.post(url, param);
-    }
-
-    // 平台菜单修改
-    EdiDomain = async (param) => {
-        var url = 'safe/region/updateRegionInfo';
-        return await Http.post(url, param);
-    }
-     // 平台菜单新增
-    AddDomain = async (param) => {
-        var url = 'safe/region/addRegionInfo';
-        return await Http.post(url, param);
+    //删除区域
+    deleteDomain=async(param)=>{
+        let url = 'regions/'+param.id;
+        return await Http.delete(url, param);
     }
 }
 
