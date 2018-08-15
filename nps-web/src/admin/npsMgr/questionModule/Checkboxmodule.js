d6aafa39327c7e89812ceadfa438e6fc29b13baa
liuyang <liuyang@tcloudata.com>
2018-08-15 13:58:46 +0800

    #MOD 题目管理页面

--- nps-web/src/admin/npsMgr/questionModule/Checkboxmodule.js
+++ nps-web/src/admin/npsMgr/questionModule/Checkboxmodule.js
@@ -4,28 +4,19 @@ import React from 'react';
 
 //多选
 export default class Checkboxmodule extends PureComponent {
-    constructor(props){
-        super(props);
-        this.state={
-            id:props.id,
-            title: props.title ? props.title : '多选题标题',
-            option: props.option ? props.option : ['选项1', '选项2','选项3', '选项4']
-        }
-    }
-    onChange = (e) => {
-    }
+
     render() {
-        var items = [];
-        for (var i = 0; i < this.state.option.length; i++) {
-            items.push( <Col span={24}><Checkbox value={i}>{this.state.option[i]}</Checkbox></Col>);
-        }
+        const { index, title, option } = this.props;
+
+        const items = option.map((item, key) => {
+            return <Checkbox style={{display: 'block',height: '30px',lineHeight: '30px', marginLeft: '20px'}} value={key} key={key}>{item}</Checkbox>;
+        });
+
         return (
-            <div>
-                <h3>{this.state.id}.{this.state.title}[多选题]</h3>
-                <Checkbox.Group onChange={this.onChange}>
-                    <Row>
-                        {items}
-                    </Row>
+            <div style={{padding: '5px'}}>
+                <h3>{index}、{title}[多选题]</h3>
+                <Checkbox.Group onChange={this.props.onChange}>
+                    {items}
                 </Checkbox.Group>
             </div>
         );
