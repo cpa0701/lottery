50c537bd38977886b9b7d587e187114e1ed2d5dd
huhuirong <xxx>
2018-08-15 11:43:07 +0800

    单选修改字段

--- nps-web/src/admin/npsMgr/questionModule/Radiomodule.js
+++ nps-web/src/admin/npsMgr/questionModule/Radiomodule.js
@@ -5,23 +5,13 @@ const RadioGroup = Radio.Group;
 
 //单选
 export default class RadioModule extends PureComponent {
-    constructor(props){
-        super(props);
-        this.state={
-            value: 0,
-            id: props.id,
-            title: props.title ? props.title : '单选题标题',
-            option: props.option ? props.option : ['选项1', '选项2']
-        }
-    }
     onChange = (e) => {
         this.setState({
             value: e.target.value,
         });
     };
-
     render() {
-        const {id, title, option, value} = this.state;
+        const {index, title, option, value} = this.props;
 
         const items = option.map((item, key) => {
             return <Radio style={{display: 'block',height: '30px',lineHeight: '30px', marginLeft: '20px'}} value={key} key={key}>{item}</Radio>;
@@ -29,7 +19,7 @@ export default class RadioModule extends PureComponent {
 
         return (
             <div style={{padding: '5px'}}>
-                <h3>{id}、{title}</h3>
+                <h3>{index}.{title}</h3>
                 <RadioGroup value={value} onChange={this.onChange}>
                     {items}
                 </RadioGroup>
