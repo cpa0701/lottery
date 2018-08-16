c14eb5c14009136f50a58d15f13af689c340b009
chenpengan <cpa0701@163.com>
2018-08-16 11:52:37 +0800

    修改单选模版

--- nps-web/src/admin/npsMgr/questionModule/RadioModule.js
+++ nps-web/src/admin/npsMgr/questionModule/RadioModule.js
@@ -1,22 +1,29 @@
-import React, { PureComponent } from 'react';
-import { Radio } from 'antd';
+import React, {PureComponent} from 'react';
+import {Radio} from 'antd';
 
 const RadioGroup = Radio.Group;
 
 //单选
 export default class RadioModule extends PureComponent {
     render() {
-        const {index, title, option, value} = this.props;
+        const {index, questionName, optionList} = this.props;
 
-        const items = option.map((item, key) => {
-            return <Radio style={{display: 'block',height: '30px',lineHeight: '30px', marginLeft: '20px'}} value={key} key={key}>{item}</Radio>;
+        const optionLIst = optionList.map((item) => {
+            return <Radio style={optionList.length > 3 ? {
+                display: 'block',
+                height: '30px',
+                lineHeight: '30px',
+                marginLeft: '20px'
+            } : {marginLeft: '20px'}} value={item.optionId} key={item.optionOrder}
+                          setupQuestionId={item.setupQuestionId}
+                          skiptoQuestionId={item.skiptoQuestionId}>{item.optionName}</Radio>;
         });
 
         return (
             <div style={{padding: '5px'}}>
-                <h3>{index}、{title}</h3>
-                <RadioGroup value={value} onChange={this.props.onChange}>
-                    {items}
+                <h3>{index}、{questionName}</h3>
+                <RadioGroup onChange={this.props.onChange}>
+                    {optionLIst}
                 </RadioGroup>
             </div>
         );
--- nps-web/src/admin/npsMgr/questionPreview/QuestionPreview.js
+++ nps-web/src/admin/npsMgr/questionPreview/QuestionPreview.js
@@ -2,27 +2,55 @@
  * Create by chenpengan on 2018/8/15
  */
 import React from 'react';
+import {Row, Col, Spin} from "antd"
+
 import QuestionPreviewService from '../../../services/question/QuestionPreviewService'
+import InitQuestionList from '../questionModule/InitQuestionList'
+
+import './questionPreview.less'
 
 class QuestionPreview extends React.PureComponent {
     constructor(props) {
         super(props);
         // let id = props.location.query.id;
         // console.log(id);
-        this.state = {}
+        this.state = {
+            loading: false,
+            questionList: []
+        }
     }
 
     componentWillMount() {
-        QuestionPreviewService.getQuestionList().then(result => {
-            console.log(result)
+        this.setState({
+            loading: true
+        }, () => {
+            QuestionPreviewService.getQuestionList().then(result => {
+                this.setState({
+                    loading: false,
+                    questionList: result.list,
+                    qstnaireTitle: result.qstnaireTitle
+                });
+                console.log(result)
+            })
         })
     }
 
     render() {
+        const questionnaire = this.state.questionList.map((item, i) => {
+            return <InitQuestionList type={item.questionType} key={item.questionId} index={i} questionName={item.questionName}
+                                     optionList={item.optionList} isSetup={item.isSetup}/>
+        })
         return (
-            <div>
-                我是预览
-            </div>
+            <Spin spinning={this.state.loading}>
+                <Row style={{overflow: 'auto'}} className={'question-preview'}>
+                    <Col span={24} className={'surveyhead'}>
+                        <h1>{this.state.qstnaireTitle}</h1>
+                    </Col>
+                    <Col span={12} offset={6}>
+                        {questionnaire}
+                    </Col>
+                </Row>
+            </Spin>
         )
     }
 }
