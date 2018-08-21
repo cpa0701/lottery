a61ebc0201fde70df83a380f6f6ec500e087a570
liuyang <liuyang@tcloudata.com>
2018-08-21 20:03:01 +0800

    #MOD 关联逻辑弹框

--- nps-web/src/admin/npsMgr/questionApplication/ConnModal.js
+++ nps-web/src/admin/npsMgr/questionApplication/ConnModal.js
@@ -1,14 +1,20 @@
-import React, {Component} from 'react';
-import {Modal, Form,Select,Button} from 'antd';
-import InitQuestionList from './InitQuestionList'
-const FormItem = Form.Item;
-const Option = Select.Option;
+import React, { Component } from 'react';
+import { Modal, Form, Select, Button, Row, Col, Radio, Icon, Popconfirm } from 'antd';
+
+import InitQuestionList from './InitQuestionList';
+
+import './questionApplication.less';
+
+const [FormItem, Option, RadioGroup] = [Form.Item, Select.Option, Radio.Group];
+
+let uuid = 0;
 @Form.create()
 export default class extends Component {
     state = {
         value:0,
         question:[],
     };
+
     onSubmit = () => {
         this.props.form.validateFieldsAndScroll((errors, values) => {
             if (errors) {
@@ -18,7 +24,9 @@ export default class extends Component {
         });
         this.props.onClose()
     };
+
     afterClose = () => this.props.form.resetFields();
+
     handleChange=(value)=>{
         let question = this.props.connList.filter(item => item.questionId === value);
         this.setState({
@@ -26,10 +34,84 @@ export default class extends Component {
             question,
         });
     };
+
+    // 新增关联题选框
+    add = () => {
+        const { form } = this.props;
+        const keys = form.getFieldValue('keys');
+        const nextKeys = keys.concat(uuid);
+        uuid++;
+        form.setFieldsValue({
+            keys: nextKeys,
+        });
+    };
+    // 删除关联题选框
+    remove = (k) => {
+        const { form } = this.props;
+        const keys = form.getFieldValue('keys');
+        if (keys.length === 1) {
+            return;
+        }
+        form.setFieldsValue({
+            keys: keys.filter(key => key !== k),
+        });
+    };
+
     render() {
-        const {conn,record,connList, form: {getFieldDecorator}} = this.props;
+        const { conn, index, record, connList, form: { getFieldDecorator, getFieldValue } } = this.props;
+
         const optionList = connList.map((item) => {
             return  <Option key={item.questionId} value={item.questionId}>{item.questionName}</Option>
+        });
+
+        getFieldDecorator('keys', { initialValue: [1] });
+        const keys = getFieldValue('keys');
+        const formItems = keys.map((k, index) => {
+            return (
+                <div>
+                    <FormItem
+                        labelCol={{span: 4}}
+                        wrapperCol={{span: 20}}
+                        label={`关联题目${index + 1}`}
+                        required={false}
+                        key={index}
+                    >
+                        {getFieldDecorator(`question[${k}]`, {
+                            validateTrigger: ['onChange', 'onBlur'],
+                            onChange: this.handleChange
+                        })(
+                            <Select
+                                style={{width: '90%', marginRight: 8}}
+                                placeholder="请选择要关联题目"
+                                optionFilterProp="children"
+                                filterOption={(input, option) =>
+                                    option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                                }
+                            >
+                                {optionList}
+                            </Select>
+                        )}
+                        {keys.length > 1 ? (
+                            <Icon
+                                className="dynamic-delete-button"
+                                type="minus-circle-o"
+                                disabled={keys.length === 1}
+                                onClick={() => this.remove(k)}
+                            />
+                        ) : null}
+                    </FormItem>
+                    <div style={{ padding: '0 60px', marginBottom: '10px'}}>{(this.state.value===1) ?
+                        <div>
+                            当关联题目{ index + 1 } 选择下面的选项<br/>
+                            {this.state.question.map((item, i) => {
+                                return(
+                                    <InitQuestionList questionType={item.questionType} questionId={item.questionId} key={item.questionId} index={i+1} questionName={item.questionName} optionList={item.optionList}/>
+                                )})}
+                            中的任意一个时，"当前题目"才出现
+                        </div> : ''}
+                    </div>
+                </div>
+            );
         });
 
         return(
@@ -41,26 +123,44 @@ export default class extends Component {
                 onCancel={() => this.props.onClose()}
                 afterClose={this.afterClose}
                 footer={[
+                    <Popconfirm title="你确定删除该题所有关联逻辑?" onConfirm=""><Button key="cancel" type="danger" icon="delete">删除关联逻辑</Button></Popconfirm>,
                     <Button key="submit" type="primary" icon="check-circle-o" onClick={this.onSubmit}>确定</Button>,
                 ]}
             >
-                <form>
-                    <h2>当前题目：{record.questionName}</h2>
-                    <h2>关联题目1：
-                        <Select defaultValue="请选择关联的题目" style={{ width: 220 }} onChange={this.handleChange} >
-                            {optionList}
-                    </Select>
-                    </h2>
-                    <div>{(this.state.value===1)?<div>
-                            当关联题目选择下面的选项<br/>
-                        {this.state.question.map((item,i) => {
-                            return(
-                        <InitQuestionList questionType={item.questionType} questionId={item.questionId} key={item.questionId} index={i+1} questionName={item.questionName} optionList={item.optionList}/>
-                            )})}
-                        中的任意一个时，"当前题目"才出现
-
-                    </div>:''}</div>
-                </form>
+                <Row className="jumpModal">
+                    <Col span={24} className="jumpHeader">
+                        <h2>当前题目：{index}、{record.questionName}</h2>
+                    </Col>
+                    <Col span={24} className="jumpContent">
+                        <Form className="connContent">
+                            {formItems}
+                            {this.state.value ?
+                                <FormItem
+                                    labelCol={{span: 4}}
+                                    wrapperCol={{span: 20}}
+                                    label={'关联多题时'}
+                                    required={false}
+                                    key={index}
+                                >
+                                    {getFieldDecorator('andOr', {
+                                        initialValue: '0',
+                                    })(
+                                        <RadioGroup>
+                                            多题之间 <Radio value="0">为“且”的关系(必须满足所有关联条件)</Radio>
+                                                    <Radio value="1">为“或”的关系(满足一个关联条件即可)</Radio>
+                                        </RadioGroup>
+                                    )}
+                                </FormItem>
+                                : ''
+                            }
+                            <FormItem style={{ textAlign: 'center' }}>
+                                <Button type="dashed" onClick={this.add} style={{ width: '60%' }}>
+                                    <Icon type="plus" /> 添加更多关联题
+                                </Button>
+                            </FormItem>
+                        </Form>
+                    </Col>
+                </Row>
             </Modal>
         )
     }
