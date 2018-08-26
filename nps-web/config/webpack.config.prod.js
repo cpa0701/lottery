bf9b8945c6240c43b3dda9edc5544e2089d743bc
chenpengan <cpa0701@163.com>
2018-08-26 15:34:22 +0800

    gzip打包

--- nps-web/config/webpack.config.prod.js
+++ nps-web/config/webpack.config.prod.js
@@ -76,6 +76,8 @@ const htmlPluginsAray = paths.htmlArray.map((v) => {
         },
     })
 });
+const CompressionPlugin = require("compression-webpack-plugin");
+
 // This is the production configuration.
 // It compiles slowly and is focused on producing a fast and minimal bundle.
 // The development configuration is different and lives in a separate file.
@@ -343,6 +345,15 @@ module.exports = {
             name: 'vendor',
             filename: 'vendor.bundle.js',
             minChunks: Infinity
+        }),
+        new CompressionPlugin({
+            asset: '[path].gz[query]', //目标资源名称。[file] 会被替换成原资源。[path] 会被替换成原资源路径，[query] 替换成原查询字符串
+            algorithm: 'gzip',//算法
+            test: new RegExp(
+                '\\.(js|css)$'    //压缩 js 与 css
+            ),
+            threshold: 10240,//只处理比这个值大的资源。按字节计算
+            minRatio: 0.8//只有压缩率比这个值小的资源才会被处理
         }),
         // Makes some environment variables available in index.html.
         // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
