64a2e6290cccbe56062da53917846b492b5a2cb5
chenpengan <cpa0701@163.com>
2018-08-27 19:49:20 +0800

    分页，提交，验证必填

--- nps-web/src/admin/npsMgr/questionModule/BlankModule.js
+++ nps-web/src/admin/npsMgr/questionModule/BlankModule.js
@@ -6,13 +6,15 @@ const {TextArea} = Input;
 //单项填空
 export default class BlankModule extends PureComponent {
     render() {
-        const {index, questionName, optionList, isView, questionNameBlur, onChange} = this.props;
+        const {index, questionName, optionList, isView, questionNameBlur, onChange, isBlank} = this.props;
         return (
             <div className={'radio-module'} style={{padding: '12px'}}>
                 <h3>{index}、{isView ? questionName :
-                    <Input className="titleInput" defaultValue={questionName} onBlur={(e) => this.props.questionNameBlur(e, index)}/>}
+                    <Input className="titleInput" defaultValue={questionName}
+                           onBlur={(e) => questionNameBlur(e, index)}/>}
                 </h3>
-                <TextArea className="textAreaInput" onBlur={onChange} defaultValue={optionList} autosize={{ minRows: 3, maxRows: 6 }}/>
+                <TextArea className="textAreaInput" questionindex={index} isblank={isBlank} onBlur={onChange} defaultValue={optionList}
+                          autosize={{minRows: 3, maxRows: 6}}/>
             </div>
         );
     }
