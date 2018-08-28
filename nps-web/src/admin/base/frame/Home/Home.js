59190fc20d1d3f99e0339514120cdc90d1bcae09
chenpengan <cpa0701@163.com>
2018-08-28 13:48:13 +0800

    添加echarts

--- nps-web/src/admin/base/frame/Home/Home.js
+++ nps-web/src/admin/base/frame/Home/Home.js
@@ -18,6 +18,7 @@ const columns = [{
     title: 'Age',
     dataIndex: 'age',
 }];
+const echarts = require('echarts');
 
 class Home extends React.PureComponent {
     constructor() {
@@ -35,7 +36,51 @@ class Home extends React.PureComponent {
     hideModal = () => {
         this.setState({visible: false});
     }
+    componentDidMount(){
+        // 基于准备好的dom，初始化echarts实例
+        let myChart = echarts.init(document.getElementById('example'));
+        const option = {
+            title : {
+                text: '某站点用户访问来源',
+                subtext: '纯属虚构',
+                x:'center'
+            },
+            tooltip : {
+                trigger: 'item',
+                formatter: "{a} <br/>{b} : {c} ({d}%)"
+            },
+            legend: {
+                orient: 'vertical',
+                left: 'left',
+                data: ['直接访问','邮件营销','联盟广告','视频广告','搜索引擎']
+            },
+            series : [
+                {
+                    name: '访问来源',
+                    type: 'pie',
+                    radius : '55%',
+                    center: ['50%', '60%'],
+                    data:[
+                        {value:335, name:'直接访问'},
+                        {value:310, name:'邮件营销'},
+                        {value:234, name:'联盟广告'},
+                        {value:135, name:'视频广告'},
+                        {value:1548, name:'搜索引擎'}
+                    ],
+                    itemStyle: {
+                        emphasis: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                }
+            ]
+        };
+        // 绘制图表
+        myChart.setOption(option);
 
+    }
     render() {
         const info = () => {
             Modal.info({
@@ -85,6 +130,7 @@ class Home extends React.PureComponent {
                 <Modal title="Locale Modal" visible={this.state.visible} onCancel={this.hideModal}>
                     <p>Locale Modal</p>
                 </Modal>
+                <div id="example" style={{height:500}}/>
             </div>
         );
     }
