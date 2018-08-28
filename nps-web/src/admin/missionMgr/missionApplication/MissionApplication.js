b74dc525c4bff5fbac60de67d840d690bde8cf4f
liuyang <liuyang@tcloudata.com>
2018-08-28 17:14:24 +0800

    #MOD 调研任务申请页

--- nps-web/src/admin/missionMgr/missionApplication/MissionApplication.js
+++ nps-web/src/admin/missionMgr/missionApplication/MissionApplication.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import {Row, Col, Tabs, Button, Input, Menu, Dropdown, Icon, Pagination, Spin} from "antd"
+import {Row, Col, Tabs, Button, Input, Popconfirm, Icon, Pagination, Spin} from "antd"
 import TaskResearchService from "../../../services/research/TaskResearchService"
 import './missionApplication.less'
-import { Collapse } from 'antd';
-const Panel = Collapse.Panel;
+
 const Search = Input.Search;
 const TabPane = Tabs.TabPane;
 
@@ -12,132 +11,131 @@ class MissionApplication extends React.PureComponent {
         super(props);
         this.state = {
             loading: false,
-            questionnaireList: [],
-            questionnaireName: '',
+            taskList: [],
+            taskName: '',
             pageNum: 1,
             total: 0
-        }
+        };
         this.createRequisition = this.createRequisition.bind(this);
     }
 
    componentWillMount() {
-        this.getQuestionnaireList()
+        this.getMissionList()
     }
 
     // 获取申请单列表
-    getQuestionnaireList = (param) => {
+    getMissionList = (param) => {
         let params = {
-            questionnaireName: this.state.questionnaireName,
-            questionType: this.state.questionType,
-            questionBusiness: this.state.questionBusiness,
+            taskName: this.state.taskName,
             ...param
-        }
+        };
        this.setState({
             loading: true
-        }, () => TaskResearchService.getTaskList(params).then(result => {
+        }, () => TaskResearchService.getMissionList(params).then(result => {
             this.setState({
-                questionnaireList: result.list,
-                pageNum: result.pageNum,
-                total: result.total,
+                taskList: result.list,
+                total: result.totalCount,
                 loading: false
             })
         }))
-    }
+    };
+
     //新建申请单
     createRequisition = () => {
         this.props.history.push('/missionMgr/newApplicationForm');
-    }
-    // 点击流程图
-    //handleMenuClick = (key, a, b) => {
-       // console.log(key, a, b)
-    //}
+    };
+
     //tab标签被点击
     onTabClick = (key) => {
         this.setState({
-            questionnaireList: []
-        }, () => this.getQuestionnaireList({catalogId: key}))
-    }
+            taskList: []
+        }, () => this.getMissionList({status: key}))
+    };
+
     //输入框输入
     onSearch = (value) => {
         this.setState({
-            questionnaireName: value
-        }, () => this.getQuestionnaireList())
-    }
+            taskName: value
+        }, () => this.getMissionList())
+    };
+
     //列表分页
     refreshList = (page) => {
-        this.getQuestionnaireList({pageNum: page,});
-    }
+        this.getMissionList({pageNum: page,});
+    };
 
     render() {
+        const { taskList, total } = this.state;
+
         const operations = <Search
             placeholder="在结果中查询"
             onSearch={value => this.onSearch(value)}
             enterButton
-
         />;
-        //流程图
-        function callback(key) {
-            console.log(key);
-        }
-        const text = `
-                  A dog is a type of domesticated animal.
-                  Known for its loyalty and faithfulness,
-                  it can be found as a welcome guest in many households across the world.
-                `;
-        const menu = (
-            //审核日志
-            <Menu onClick={this.handleMenuClick}>
-                <Menu.Item key="1">1st item</Menu.Item>
-                <Menu.Item key="2">2nd item</Menu.Item>
-                <Menu.Item key="3">3rd item</Menu.Item>
-            </Menu>
-        );
-        const {questionnaireList} = this.state;
         const questionLIst = <div>
-            <Spin spinning={this.state.loading}>
-                {questionnaireList.map(item => {
-                    return (<div key={item.qstnaireId} className={'sub-li'}>
-                                <Row type="flex" justify="space-between">
-                                    <Col span={16} className={'subject-name'}>{item.qstnaireTitle}
-                                    <br/>
-                                    <span className={'c1'}>测试结果:测试成功，已确认</span>
-                                    </Col>
-                                    <Col span={8}>
-                                        <Button type="primary">查看</Button>
-                                        <Button type="primary">预览</Button>
-                                        <Button >
-                                            流程图 <Icon type="down"/>
-                                        </Button>
-                                    </Col>
-                                </Row>
-                                <Row type="flex" justify="start">
-                                    <Col span={3}><Icon type="appstore" style={{marginRight: '5px' ,color:'#88d7fd'}}/>问卷分类：{item.catalogId}</Col>
-                                    <Col span={3}><Icon type="retweet" style={{marginRight: '5px'}}/>适用渠道：{item.channelId}</Col>
-                                    <Col span={3}><Icon type="ant-design" style={{marginRight: '5px'}}/>任务状态：{item.status}</Col>
-                                    <Col span={3}><Icon type="clock-circle" style={{marginRight: '5px' ,color:'#fecb45'}}/>申请时间：{item.createUid}</Col>
-                                    <Col span={6}><Icon type="eye-o" style={{marginRight: '5px'}}/>调研数： {item.createTime}</Col>
-                                </Row>
+                                <Spin spinning={this.state.loading}>
+                                    {taskList.map((item, k) => {
+                                        return (<div key={k} className="sub-li">
+                                                    <Row type="flex" justify="space-between">
+                                                        <Col span={16} className="subject-name">{item.taskName}
+                                                        <br/>
+                                                        <span className="c1">测试结果:{ item.testFlag }</span>
+                                                        </Col>
+                                                        <Col span={8} style={{textAlign: 'right', paddingRight: '40px'}}>
+                                                            <Button type="primary">查看</Button>
+                                                            {item.status !== '05'? '' :
+                                                                <div style={{ display: 'inline-block'}}>
+                                                                    <Button type="primary">编辑</Button>
+                                                                    <Popconfirm title="你确定删除该任务?" onConfirm={() => this.delTask(item.taskId)}>
+                                                                        <Button type="primary">删除</Button>
+                                                                    </Popconfirm>
+                                                                </div>
+                                                            }
+                                                            <Button type="primary">预览</Button>
+                                                            {item.status !== '02'? '' :
+                                                                    <Popconfirm
+                                                                        title="该任务是否走审批流程?"
+                                                                        onCancel={() => this.delTask(item.taskId)}
+                                                                        okText={'是'}
+                                                                        cancelText={'否'}
+                                                                    >
+                                                                        <Button type="primary">审批流程</Button>
+                                                                    </Popconfirm>
+                                                            }
+                                                            {/*<Button >*/}
+                                                                {/*流程图 <Icon type="down"/>*/}
+                                                            {/*</Button>*/}
+                                                        </Col>
+                                                    </Row>
+                                                    <Row type="flex" justify="start">
+                                                        <Col span={3}><Icon type="appstore" style={{marginRight: '5px' ,color:'#88d7fd'}}/>问卷分类：{item.catalogName}</Col>
+                                                        <Col span={3}><Icon type="retweet" style={{marginRight: '5px'}}/>适用渠道：{item.channelName}</Col>
+                                                        <Col span={3}><Icon type="ant-design" style={{marginRight: '5px'}}/>任务状态：{item.status}</Col>
+                                                        <Col span={5}><Icon type="clock-circle" style={{marginRight: '5px' ,color:'#fecb45'}}/>申请时间：{item.createTime}</Col>
+                                                        <Col span={5}><Icon type="eye-o" style={{marginRight: '5px'}}/>调研数： {item.survey_count}</Col>
+                                                    </Row>
+                                                </div>)
+                                    })}
+                                </Spin>
+                                <Pagination current={this.state.pageNum} onChange={this.refreshList} total={this.state.total} showQuickJumper/>
+                            </div>;
+        let tab1Title = "我的申请单( 共" + total + "条 )";
+        let tab2Title = "审批中( 共" + total + "条 )";
+        let tab3Title = "发布中( 共" + total + "条 )";
+        let tab4Title = "草稿( 共" + total + "条 )";
 
-                        </div>
-
-                    )
-                })}
-            </Spin>
-            <Pagination current={this.state.pageNum} onChange={this.refreshList} total={this.state.total}
-                        showQuickJumper/>
-        </div>
         return (
-            <div className={'requisition '}>
-                <Button type="primary" className={'newrequisition'} icon="plus" onClick={this.createRequisition}>
+            <div className='missionApplication '>
+                <Button type="primary" icon="plus" onClick={this.createRequisition}>
                     新建申请单
                 </Button>
                 <Tabs tabBarExtraContent={operations} onTabClick={this.onTabClick}>
-                    <TabPane tab="我的申请单" key="1">{questionLIst}</TabPane>
-                    <TabPane tab="审批中" key="2">{questionLIst}</TabPane>
-                    <TabPane tab="已发布" key="3">{questionLIst}</TabPane>
-                    <TabPane tab="审核否决" key="4">{questionLIst}</TabPane>
-                    <TabPane tab="审核作废" key="5">{questionLIst}</TabPane>
-                    <TabPane tab="草稿" key="6">{questionLIst}</TabPane>
+                    <TabPane tab={tab1Title} key="00">{questionLIst}</TabPane>
+                    <TabPane tab={tab2Title} key="01">{questionLIst}</TabPane>
+                    <TabPane tab={tab3Title} key="02">{questionLIst}</TabPane>
+                    <TabPane tab="审核否决(0)" key="03">{questionLIst}</TabPane>
+                    <TabPane tab="审核作废(0)" key="04">{questionLIst}</TabPane>
+                    <TabPane tab={tab4Title} key="05">{questionLIst}</TabPane>
                 </Tabs>
             </div>
         )
