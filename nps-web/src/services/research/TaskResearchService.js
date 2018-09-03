727af12d3c2b3947d845496557ec854e78328360
liuyang <liuyang@tcloudata.com>
2018-09-03 14:41:15 +0800

    #MOD 调研任务终止，周期性调研页面及逻辑

--- nps-web/src/services/research/TaskResearchService.js
+++ nps-web/src/services/research/TaskResearchService.js
@@ -2,8 +2,13 @@ import Http from '../../common/Http';
 class TaskResearchService {
     //获取调研任务列表
     getMissionList = async (param) => {
-        var url = 'surveyTaskMgr/surveyTaskList';
+        let url = 'surveyTaskMgr/surveyTaskList';
         // let url = 'mock/missionMgr/getMissionList';
+        return await Http.post(url, param);
+    };
+    // 结束调研
+    stopMission = async (param) => {
+        let url = 'mock/missionMgr/stopMission';
         return await Http.post(url, param);
     }
 }
