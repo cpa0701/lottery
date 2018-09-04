31b0afd772dfe5ab175e73cc4b018505c5923ade
tang.yujun <tang.yujun2@ztesoft.com>
2018-09-04 14:58:20 +0800

    oauth2添加

---
+++ nps-int/src/main/java/com/ztesoft/nps/common/oauth2/authorization/OAuth2AuthorizationServer.java
@@ -0,0 +1,67 @@
+package com.ztesoft.nps.common.oauth2.authorization;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableAuthorizationServer
+public class OAuth2AuthorizationServer extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey("ztesoft");
+        return converter;
+    }
+
+    @Bean
+    public JwtTokenStore jwtTokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public TokenEnhancer tokenEnhancer() {
+        return new CustomTokenEnhancer();
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+        tokenEnhancerChain.setTokenEnhancers(
+                Arrays.asList(tokenEnhancer(), accessTokenConverter()));
+
+        endpoints
+            .authenticationManager(authenticationManager)
+            .tokenStore(jwtTokenStore())
+            .tokenEnhancer(tokenEnhancerChain)
+            .accessTokenConverter(accessTokenConverter());
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+            .withClient("client-ztesoft")
+            .secret("123456")
+            .scopes("nps-api")
+            .authorizedGrantTypes(
+                "password",
+                //"authorization_code",
+                "refresh_token");
+    }
+
+}
---
+++ nps-int/src/main/java/com/ztesoft/nps/common/oauth2/resource/OAuth2ResourceServer.java
@@ -0,0 +1,29 @@
+package com.ztesoft.nps.common.oauth2.resource;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableResourceServer
+public class OAuth2ResourceServer extends
+    ResourceServerConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+//        http
+//            .authorizeRequests()
+//                .anyRequest().authenticated().antMatchers( "/oauth/**","/swagger-ui.html").permitAll()
+//                //.and().formLogin().loginPage("/login")
+//                //.and().logout().logoutSuccessUrl("/")
+//                .and().requestMatchers().antMatchers("/**");
+        http.antMatcher("/**")        //匹配需要资源认证路径
+                .authorizeRequests()
+                .antMatchers("/swagger-ui.html", "/swagger-resources/**",
+                        "/v2/api-docs/**", "/validatorUrl","/valid"
+                ).permitAll().anyRequest().authenticated();           //匹配不需要资源认证路径
+
+
+    }
+
+}
