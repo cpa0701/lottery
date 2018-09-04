16f1897f86e27feea619647fa1b149c14606635e
fanxm <fan.xiaoming@ztesoft.com>
2018-09-04 14:03:27 +0800

    任务接口调试

--- nps-int/src/main/java/com/ztesoft/nps/business/surveyTaskMgr/model/query/SurveyTaskIdQuery.java
+++ nps-int/src/main/java/com/ztesoft/nps/business/surveyTaskMgr/model/query/SurveyTaskIdQuery.java
@@ -1,10 +1,12 @@
 package com.ztesoft.nps.business.surveyTaskMgr.model.query;
 
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 /**
  * Created by 64671 on 2018/8/29.
  */
+@ApiModel
 public class SurveyTaskIdQuery {
 
     @ApiModelProperty("调研任务ID")
